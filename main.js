034ff940dd94d50a7b6b6ab013d325e7df02afd0
thamyriscardoso <thamy_cardosoo@hotmail.com>
2018-08-29 11:44:00 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -16,3 +16,14 @@ function callback(pos){
     console.dir(pos);
     p.textContent = `${pos.coords.latitude}, ${pos.coords.longitude} acurácia ${pos.coords.accuracy}`;
 }
+
+//speech
+
+const bt3 = document.querySelectorAll('button')[2];
+const input2 = document.querySelectorAll('input')[1];
+
+bt3.addEventListener('click',function(){
+    const texto = input2.value;
+    const fala = new SpeechSynthesisUtterance(texto);
+    speechSynthesis.speak(fala);
+})
